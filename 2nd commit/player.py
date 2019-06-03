bf8be747b8a59bdd97469e8c9943498116baacdc
Daniel Hernandez <danihcg.23@gmail.com>
2019-06-02 21:10:54 -0700

    Revert "Revert "The is the second and final commit of my final project.""
    This reverts commit e527c5ea5f4d048999625a083de4ca12badd6cf6.

--- 2nd commit/player.py
+++ 2nd commit/player.py
@@ -1,39 +1,95 @@
 class Player:
-    pass
 
-player1 = Player()
-player2 = Player()
-player3 = Player()
+    # Creates 1st instance of Player class
+    def __init__(self, name, obp, slg):
+        self.name = name
+        self.obp = obp
+        self.slg = slg
 
-print('\n')
-player1.name = str(input("Player name: "))
-print(player1.name)
-player1.obp = float(input("Please enter " + player1.name + "'s OBP: "))
-player1.slg = float(input("Please enter " + player1.name + "'s SLG: "))
-print('\n')
 
-player2.name = str(input("Player name: "))
-print(player2.name)
-player2.obp = float(input("Please enter " + player2.name + "'s OBP: "))
-player2.slg = float(input("Please enter " + player2.name + "'s SLG: "))
-print('\n')
+# Assigns values to first player object through
+# user input
+p1 = Player(
+    str(input("Player name: ")),
+    (float(input("Please enter OBP: "))),
+    (float(input("Please enter SLG: ")))
+)
 
-player3.name = str(input("Player name: "))
-print(player3.name)
-player3.obp = float(input("Please enter " + player3.name + "'s OBP: "))
-player3.slg = float(input("Please enter " + player3.name + "'s SLG: "))
-print('\n')
 
+# Constant variable that are used for each players'
+# calculation.
 LG_OBP = 0.363
 LG_SLG = 0.397
 
-ops_plus1 = 100 * (player1.obp/LG_OBP + player1.slg/LG_SLG - 1)
-ops_plus2 = 100 * (player2.obp/LG_OBP + player2.slg/LG_SLG - 1)
-ops_plus3 = 100 * (player3.obp/LG_OBP + player3.slg/LG_SLG - 1)
 
-lyst = [round(ops_plus1), round(ops_plus2), round(ops_plus3)]
-lyst.sort(reverse=True)
+# OPS+ calculation
+ops_plus1 = 100 * (p1.obp/LG_OBP + p1.slg/LG_SLG - 1)
+print(p1.name + "'s OPS+:", round(ops_plus1))
+print('\n')
 
+
+# Creates second instance of Player class
+def player2(self, name, obp, slg):
+    self.name = name
+    self.obp = obp
+    self.slg = slg
+
+
+# Assigns values to second player object through
+# user input
+p2 = Player(
+    str(input("Player name: ")),
+    (float(input("Please enter OBP: "))),
+    (float(input("Please enter SLG: ")))
+)
+
+
+# OPS+ calculation
+ops_plus2 = 100 * (p2.obp/LG_OBP + p2.slg/LG_SLG - 1)
+print(p2.name + "'s OPS+:", round(ops_plus2))
+print('\n')
+
+
+# Creates third instance of player class
+def player3(self, name, obp, slg):
+    self.name = name
+    self.obp = obp
+    self.slg = slg
+
+
+# Assigns values to third player object through
+# user input
+p3 = Player(
+    str(input("Player name: ")),
+    (float(input("Please enter OBP: "))),
+    (float(input("Please enter SLG: ")))
+)
+
+
+# Calculation for OPS+ and prints the result
+ops_plus3 = 100 * (p3.obp/LG_OBP + p3.slg/LG_SLG - 1)
+print(p3.name + "'s OPS+:", round(ops_plus3))
+print('\n')
+
+
+# Creates an array than can be referenced later in the for loop
+# to print the results.
+# Uses round() function to round the results from the OPS+
+# calculations.
+ops_plus = [round(ops_plus1), round(ops_plus2), round(ops_plus3)]
+
+
+# Prints the title of the leader board
 print("OPS+ Leaderboard")
-for lyst in (ops_plus1, ops_plus2, ops_plus3):
-    print(lyst)
+
+
+# Sorts the array items in descending order using the
+# sort() function
+ops_plus.sort(reverse=True)
+
+
+# For loop that displays the OPS+'s of all three players
+# and uses the enumerate function to attach a counter
+# to the the list.
+for count, value in enumerate(ops_plus, 1):
+    print(count, value)
